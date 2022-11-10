0fba9d802bb12de97b3a7fb2e9554f0a236db26a
Fraser Hanson <fraser.hanson@gmail.com>
2022-11-10 09:59:23 -0800

    remove unused configuration files

--- init.lua
+++ init.lua
@@ -22,9 +22,7 @@ require "config.dap"
 
 vim.cmd 'colorscheme onedark'
 
--- vim.keymap.set('n', '<space>f', require'scripts'.send_to_tmux)
--- vim.keymap.set('n', '<leader>f', function() require'scripts'.send_to_tmux(true) end)
--- vim.keymap.set('n', '<c-q>', require'scripts'.quickfix_toggle)
+vim.keymap.set('n', '<c-q>', require'scripts'.quickfix_toggle)
 vim.keymap.set('n', '<space>og', require'finders'.git)
 vim.keymap.set('n', '<space>oo', require'finders'.find)
 vim.keymap.set('n', '<space>ob', '<cmd> Telescope buffers<cr>')
@@ -49,21 +47,6 @@ vim.cmd([[nmap <leader>t <Plug>PlenaryTestFile]])
 vim.cmd 'hi LspReferenceText gui=italic guibg=#393e46'
 vim.cmd 'hi LspReferenceRead gui=italic guibg=#393e46'
 vim.cmd 'hi LspReferenceWrite gui=italic guibg=#393e46'
-
-function underscore(self)
-  return self:gsub("([A-Z]+)([A-Z][a-z])", "%1_%2"):
-              gsub("([a-z%d])([A-Z])", "%1_%2"):
-              gsub("[ \t]", "_"):
-              lower()
-end
-
-function camelize(self)
-  return underscore(self):gsub("[ \t_%-](.)", string.upper)
-end
-
-function capitalize(self)
-  return self:lower():gsub("^%l", string.upper)
-end
 
 -- return true if file exists and is readable
 function file_exists(name)
