14dc160bee2a30c0f7e0bf2dd5f79094cc4a16c0
Fraser Hanson <fraser.hanson@gmail.com>
2022-11-27 09:49:36 -0800

    Restore C-l mapping to default.

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -8,4 +8,6 @@ vim.cmd('nnoremap <C-e> <cmd>lua require("harpoon.ui").toggle_quick_menu()<CR>')
 vim.cmd('nnoremap <C-h> <cmd>lua require("harpoon.ui").nav_file(1)<CR>')
 vim.cmd('nnoremap <C-j> <cmd>lua require("harpoon.ui").nav_file(2)<CR>')
 vim.cmd('nnoremap <C-k> <cmd>lua require("harpoon.ui").nav_file(3)<CR>')
-vim.cmd('nnoremap <C-l> <cmd>lua require("harpoon.ui").nav_file(4)<CR>')
+
+-- don't remap C-l, it must redraw and clear nohlsearch
+-- vim.cmd('nnoremap <C-l> <cmd>lua require("harpoon.ui").nav_file(4)<CR>')
